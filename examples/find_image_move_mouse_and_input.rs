0c4a6d7788568fb487a21c43939ac6f21de14093
Andrey Zgarbul <zgarbul.andrey@gmail.com>
2025-03-28 09:57:03 +0300

    clean unwrap in examples

--- examples/find_image_move_mouse_and_input.rs
+++ examples/find_image_move_mouse_and_input.rs
@@ -1,8 +1,8 @@
 // use rustautogui;
 
-use rustautogui::imgtools;
+use rustautogui::{errors::AutoGuiError, imgtools};
 
-fn main() {
+fn main() -> Result<(), AutoGuiError> {
     // initialize autogui
     let mut gui = rustautogui::RustAutoGui::new(false).unwrap();
 
@@ -17,8 +17,7 @@ fn main() {
             Some((0, 0, 500, 300)),
             rustautogui::MatchMode::FFT,
             None,
-        )
-        .unwrap();
+        )?;
     }
     // or another way to prepare template
     {
@@ -28,13 +27,11 @@ fn main() {
             Some((0, 0, 700, 500)),
             rustautogui::MatchMode::Segmented,
             Some(5000),
-        )
-        .unwrap();
+        )?;
     }
 
     // or segmented variant with no region
-    gui.prepare_template_from_file("test.png", None, rustautogui::MatchMode::FFT, Some(5000))
-        .unwrap();
+    gui.prepare_template_from_file("test.png", None, rustautogui::MatchMode::FFT, Some(5000))?;
 
     // change prepare template settings, like region, matchmode or max segments
 
@@ -45,46 +42,45 @@ fn main() {
     );
 
     // automatically move mouse to found template position, execute movement for 1 second
-    gui.find_image_on_screen_and_move_mouse(0.9, 1.0).unwrap();
+    gui.find_image_on_screen_and_move_mouse(0.9, 1.0)?;
 
     //move mouse to position (in this case to bottom left of stanard 1920x1080 monitor), move the mouse for 1 second
-    gui.move_mouse_to_pos(1920, 1080, 1.0).unwrap();
+    gui.move_mouse_to_pos(1920, 1080, 1.0)?;
 
     // execute left click, move the mouse to x (500), y (500) position for 1 second, release mouse click
     // used for actions like moving icons or files
     // suggestion: dont use very small moving time values, especially on mac
-    gui.drag_mouse(500, 500, 1.0).unwrap();
+    gui.drag_mouse(500, 500, 1.0)?;
 
     // execute mouse left click
-    gui.left_click().unwrap();
+    gui.left_click()?;
 
     // execute mouse right click
-    gui.right_click().unwrap();
+    gui.right_click()?;
 
     // execute mouse middle click
-    gui.middle_click().unwrap();
+    gui.middle_click()?;
 
     // execute a double (left) mouse click
-    gui.double_click().unwrap();
+    gui.double_click()?;
 
     // mouse scrolls
-    gui.scroll_down().unwrap();
-    gui.scroll_up().unwrap();
-    gui.scroll_right().unwrap();
-    gui.scroll_left().unwrap();
+    gui.scroll_down()?;
+    gui.scroll_up()?;
+    gui.scroll_right()?;
+    gui.scroll_left()?;
 
     // input keyboard string
-    gui.keyboard_input("test.com").unwrap();
+    gui.keyboard_input("test.com")?;
 
     // press a keyboard command, in this case enter(return)
-    gui.keyboard_command("return").unwrap();
+    gui.keyboard_command("return")?;
 
     // two key press
-    gui.keyboard_multi_key("alt", "tab", None).unwrap();
+    gui.keyboard_multi_key("alt", "tab", None)?;
 
     // three key press
-    gui.keyboard_multi_key("shift", "control", Some("e"))
-        .unwrap();
+    gui.keyboard_multi_key("shift", "control", Some("e"))?;
 
     // maybe you would want to loop search until image is found and break the loop then
     loop {
@@ -94,4 +90,6 @@ fn main() {
             None => (),
         }
     }
+
+    Ok(())
 }
